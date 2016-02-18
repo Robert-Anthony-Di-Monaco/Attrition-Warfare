0fc8c3a68d5ec71dd229a5bf18967f48348f9f75
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-02-18 05:41:04 -0500

    Terrain

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+	Derek put your camera code here. I have a basic follow the player camera implemented. 
+		-Rob
+*/
+
+public class CameraController : MonoBehaviour
+{
+	public Transform player; 
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		transform.position = new Vector3 (player.position.x, player.position.y + 2f, player.position.z - 20f);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+	DO NOT PLACE ANY AI CODE IN THIS SCRIPT.
+		-ROB
+*/
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 5f; 
+
+	void Start () 
+	{
+		//transform.position = new Vector3 (500f, 10f, 500f);
+	}
+
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical) * speed * Time.deltaTime;
+		transform.Translate(movement.x, 0, movement.z);
+	}
+}
