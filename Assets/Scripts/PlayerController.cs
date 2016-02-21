0c3b86ca13887097a827c82ed1934cd37db92d75
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-02-21 03:17:39 -0500

    NavMesh tests

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,21 @@ using System.Collections;
 public class PlayerController : MonoBehaviour 
 {
 	public float speed = 5f; 
+	public Transform NavMeshTarget;
+	NavMeshAgent thisGameObject;
 
 	void Start () 
 	{
-		//transform.position = new Vector3 (500f, 10f, 500f);
+		thisGameObject = GetComponent<NavMeshAgent>();
 	}
 
 	void Update () 
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical) * speed * Time.deltaTime;
-		transform.Translate(movement.x, 0, movement.z);
+//		float moveHorizontal = Input.GetAxis ("Horizontal");
+//		float moveVertical = Input.GetAxis ("Vertical");
+//		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical) * speed * Time.deltaTime;
+//		transform.Translate(movement.x, 0, movement.z);
+
+		thisGameObject.SetDestination (NavMeshTarget.position);
 	}
 }
