3c70a63deb84b07f9b411e60021068c1a05ed4d7
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-03-14 18:54:00 -0400

    script management

---
+++ Assets/All Project Scripts/AI_Scripts/Climber AI/AI_Climber.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Random = UnityEngine.Random;
+using Coroutine = System.Collections.IEnumerator;
+using BTCoroutine = System.Collections.Generic.IEnumerator<BTNodeResult>;
+
+public class AI_Climber : MonoBehaviour 
+{
+	private BehaviorTree bt;
+	
+	void Awake () 
+	{
+		InitBT();
+		bt.Start();
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	private void InitBT()
+	{
+		bt = new BehaviorTree(Application.dataPath + "/Behaviour_Mutant.xml", this);
+	}
+}
+
+
+
+/*
+	// CONDITION TEMPLATE
+	[BTLeaf(" ")]
+	public bool exampleFunction()
+	{
+		return true;
+	}
+
+	
+	// LEAF TEMPLATE
+	[BTLeaf(" ")]
+	public BTCoroutine exampleFunction()
+	{
+		yield return BTNodeResult.Success;			
+		yield return BTNodeResult.NotFinished;
+		yield return BTNodeResult.Failure;
+	}
+ */
+	
+	
+	
+
+
