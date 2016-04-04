918daebef5a0768d69b28f74a5680a13ac59fd1f
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-04-04 14:50:16 -0400

    World map fixes + asset management

---
+++ Assets/RobertsTestingScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobertsTestingScript : MonoBehaviour {
+	
+	public Transform target;
+	
+	private NavMeshAgent agent;
+	
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination (target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
