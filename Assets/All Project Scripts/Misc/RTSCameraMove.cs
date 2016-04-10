fc077e672b7f93844d400e6e5f708024a423b927
virgilgrob <virgile.grob@yahoo.com>
2016-04-10 19:17:38 -0400

    Camera upgrade: face any direction, zoom, smooth movement
    Updated each of the scenes using the camera, and made some changes to
    the main scene

--- Assets/All Project Scripts/Misc/RTSCameraMove.cs
+++ Assets/All Project Scripts/Misc/RTSCameraMove.cs
@@ -2,8 +2,13 @@
 using System.Collections;
 
 public class RTSCameraMove : MonoBehaviour {
-    public float camSpeed;
-    public float GUIsize;
+    public float camSpeed = 250;
+    public float GUIsize = 100;
+	public float cameraDistance = 250;
+	public float maxCameraDistance = 400;
+	public float minCameraDistance = 100;
+	public float cameraZoomFactor = 25;
+
     Rect bottomRect;
     Rect topRect;
     Rect leftRect;
@@ -12,35 +17,60 @@ public class RTSCameraMove : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-
-        GUIsize = 20f;
-        camSpeed = 0.75f;
-        bottomRect = new Rect(0, 0, Screen.width, GUIsize);
-        topRect = new Rect(0, Screen.height - GUIsize, Screen.width, GUIsize);
+		//point (0, 0) on the screen is the top left corner
+        topRect = new Rect(0, 0, Screen.width, GUIsize);
+        bottomRect = new Rect(0, Screen.height - GUIsize, Screen.width, GUIsize);
         leftRect = new Rect(0,0, GUIsize, Screen.height);
         rightRect = new Rect(Screen.width - GUIsize, 0, GUIsize, Screen.height);
+
+		//Only have to set the camera's starting position in the inspector, not the rotation
+		transform.forward = (transform.parent.position - transform.position).normalized;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
 
-        if (bottomRect.Contains(Input.mousePosition))
+		transform.position = transform.parent.position - transform.forward * cameraDistance;
+
+		if (bottomRect.Contains(Input.mousePosition) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.parent.Translate(camSpeed, 0, 0, Space.World);
+			//Moves faster when closer to the edge of the screen
+			float amount = (Input.mousePosition.y - (Screen.height - GUIsize)) / GUIsize; 
+
+			//Translates the vector to world space so the camera can face any direction
+			transform.parent.position += transform.parent.TransformVector (new Vector3(0, 0, amount * camSpeed * Time.deltaTime));
+
+			//transform.parent.Translate(camSpeed, 0, 0, Space.World);
         }
-        if (topRect.Contains(Input.mousePosition))
+		if (topRect.Contains(Input.mousePosition) || Input.GetKey(KeyCode.UpArrow))
         {
-            this.transform.parent.Translate(-camSpeed, 0, 0, Space.World);
+			float amount = 1 - Input.mousePosition.y / GUIsize;
+			transform.parent.position += transform.parent.TransformVector (new Vector3(0, 0, amount * -camSpeed * Time.deltaTime));
+            //transform.parent.Translate(-camSpeed, 0, 0, Space.World);
         }
-        if (rightRect.Contains(Input.mousePosition))
+		if (rightRect.Contains(Input.mousePosition) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.parent.Translate(0, 0, camSpeed, Space.World);
+			float amount = (Input.mousePosition.x - (Screen.width - GUIsize)) / GUIsize;
+			transform.parent.position += transform.parent.TransformVector (new Vector3(amount * camSpeed * Time.deltaTime, 0, 0));
+            //transform.parent.Translate(0, 0, camSpeed, Space.World);
         }
-        if (leftRect.Contains(Input.mousePosition))
+		if (leftRect.Contains(Input.mousePosition) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.parent.Translate(0, 0, -camSpeed, Space.World);
+			float amount = 1 - Input.mousePosition.x / GUIsize;
+			transform.parent.position += transform.parent.TransformVector (new Vector3(amount * -camSpeed * Time.deltaTime, 0, 0));
+            //transform.parent.Translate(0, 0, -camSpeed, Space.World);
         }
+
+		//Zoom using the fields at the top of the script
+		float mouseWheelInput = Mathf.Clamp(Input.GetAxis ("Mouse ScrollWheel"), -1, 1);
+		if (mouseWheelInput < 0 && cameraDistance < maxCameraDistance)
+			cameraDistance += cameraZoomFactor;
+		else if(mouseWheelInput > 0 && cameraDistance > minCameraDistance)
+			cameraDistance -= cameraZoomFactor;
+			
+			
+
         //manageHeight();
 	}
 
