3a797340284bd41160bfff97d54e3ab794ce076e
Derek Scheunemann <Derek.scheunemann@gmail.com>
2016-04-12 10:51:50 -0400

    PlayerHealthBar script

---
+++ Assets/PlayerHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealthBar : MonoBehaviour {
+
+
+    public GameObject worldController;
+    public GameObject player;
+    public int health;
+	// Use this for initialization
+	void Start () {
+
+        player = worldController.GetComponent<WorldController>().thePlayer.gameObject;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updateHealthBar();
+	}
+
+    void updateHealthBar()
+    {
+        health = player.GetComponent<Player_AI>().health;
+        this.transform.localScale = new Vector3(health/100, 1, 1);
+    }
+}
