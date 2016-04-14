a7344d67dec5f7bede9d7e3735f4de76fa870bb9
Robert-Anthony-Di-Monaco <rob75dimonaco@gmail.com>
2016-04-14 14:28:09 -0400

    Small fix lazers

--- Assets/PlayerAssets/LazerShot/LazerShot.cs
+++ Assets/PlayerAssets/LazerShot/LazerShot.cs
@@ -2,9 +2,6 @@
 using System.Collections;
 
 /*
-	For now this script just creates then destroys the shot
-
-	Derek: add this when you want to use the shot  
 		GameObject shotInstant = Instantiate(lazerShotPrefab, spawnPoint, player.transform.rotation) as GameObject;
 		shotInstant.getComponent<LazerShot>().Fire(target);
  */
@@ -30,12 +27,12 @@ public class LazerShot : MonoBehaviour
 
 	public void Fire(Vector3 target)
 	{
-		float shotVelocity = 250f;
+		float shotVelocity = 1000f;
 		this.gameObject.GetComponent<Rigidbody>().AddForce(shotVelocity * transform.forward, ForceMode.VelocityChange);
 
 		// Destroy shot when it hits target
 		Vector3 dir = target - transform.position;
-		float time2Target = dir.magnitude / shotVelocity;
+		float time2Target = Vector3.Distance(transform.position, target)/ shotVelocity;
 		Destroy (this.gameObject, time2Target); 
 	}
 
