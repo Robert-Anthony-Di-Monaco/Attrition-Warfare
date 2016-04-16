73d62d8a97a93fd97ae44ba7676c9450a63710af
virgilgrob <virgile.grob@yahoo.com>
2016-04-15 21:50:13 -0400

    Victory/Defeat splash screens
    Go into the world controller and check the box "victory test" or "defeat
    test" to see what it looks like. Change the X and Y values in the
    inspector to move the picture, it probably won't scale to your
    resolution perfectly.

--- Assets/All Project Scripts/WorldController.cs
+++ Assets/All Project Scripts/WorldController.cs
@@ -11,9 +11,9 @@ using System.Collections.Generic;
 public class WorldController : MonoBehaviour 
 {
 	// The Player
-	public Transform thePlayer;
-
-    public AudioSource ambient1, ambient2, ambient3;
+	public Transform thePlayer;   				 
+	//Transform playerBase,
+	//		 enemyBase;
 
 	// Holds all Player Units   
 	public List<List<Transform>> playerSquads = new List<List<Transform>>();  // a list of squads and each squad is just a list of its units
@@ -31,8 +31,18 @@ public class WorldController : MonoBehaviour
 	//All squads in the game
 	public int totalSquadsInstantiated = 0;
 	public List<Squad> allSquads = new List<Squad>();
-	
+
+	//Victory/defeat
 	int allyCrystalsDestroyed = 0, enemyCrystalsDestroyed = 0;
+
+	public Texture2D victorySplash;
+	public Texture2D defeatSplash;
+	public int XmoveSplashCenter = 0;
+	public int YmoveSplashCenter = 0;
+	public bool testVictory = false;
+	public bool testDefeat = false;
+
+	GUIStyle blackBG;
 	
 	void Start () 
 	{
@@ -41,19 +51,15 @@ public class WorldController : MonoBehaviour
 
 	void Update () 
 	{
-        if (ambient1.isPlaying == false && ambient2.isPlaying == false && ambient3.isPlaying == false)
-        {
-            int selection = Random.Range(1, 4);
-            if (selection == 1) ambient1.Play();
-            else if (selection == 2) ambient2.Play();
-            else ambient3.Play();
-        }
+		
 	}
 	
 	public void victory()
 	{
 		if(++enemyCrystalsDestroyed == 2)
 		{
+			foreach (Unit_Base unit in allUnits)
+				unit.GetComponent<HealthBar> ().enabled = false;
 			// put winning code here for splash screen or whatever
 			Debug.Log("You win! Congratulations on having no life!");
 		}
@@ -63,10 +69,54 @@ public class WorldController : MonoBehaviour
 	{
 		if(++allyCrystalsDestroyed == 2)
 		{
+			foreach (Unit_Base unit in allUnits)
+				unit.GetComponent<HealthBar> ().enabled = false;
 			// put losing code here for splash screen or whatever
 			Debug.Log("You lose! You must suck!");
 		}
 	}
-	
-	
+
+	void OnGUI(){
+		InitStyles ();
+
+		if (testVictory || enemyCrystalsDestroyed >= 2) {
+
+			//disable healthbars displaying on top of the splash screen
+			foreach (Unit_Base unit in allUnits)
+				unit.GetComponent<HealthBar> ().enabled = false;
+			
+			GUI.Box (new Rect (-1, -1, Screen.width + 1, Screen.height + 1), ".", blackBG);
+			GUI.Box (new Rect (Screen.width / 2 - 612 + XmoveSplashCenter, Screen.height / 2 - 256 + YmoveSplashCenter, Screen.width / 2 + 512, Screen.height / 2 + 256), victorySplash);
+		} else if (testDefeat || allyCrystalsDestroyed >= 2) {
+
+			//disable healthbars displaying on top of the splash screen
+			foreach (Unit_Base unit in allUnits)
+				unit.GetComponent<HealthBar> ().enabled = false;
+			
+			GUI.Box (new Rect (-1, -1, Screen.width + 1, Screen.height + 1), ".", blackBG);
+			GUI.Box (new Rect (Screen.width / 2 - 612 + XmoveSplashCenter, Screen.height / 2 - 256 + YmoveSplashCenter, Screen.width / 2 + 512, Screen.height / 2 + 256), defeatSplash);
+		}
+	}
+	Texture2D MakeTex( int width, int height, Color col )
+	{
+		Color[] pix = new Color[width * height];
+		for( int i = 0; i < pix.Length; ++i )
+		{
+			pix[ i ] = col;
+		}
+		Texture2D result = new Texture2D( width, height );
+		result.wrapMode = TextureWrapMode.Repeat;
+		result.SetPixels( pix );
+		result.Apply();
+		return result;
+	}
+	void InitStyles()
+	{
+		if( blackBG == null )
+		{
+			blackBG = new GUIStyle( GUI.skin.box );
+			blackBG.normal.background = MakeTex( 1, 1, new Color( 0f, 0f, 0f, 1f ) );
+		}
+			
+	}
 }
